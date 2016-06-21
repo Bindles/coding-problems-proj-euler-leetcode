a6b789db23bec1b25b54246dc07e0b80608fc3e0
Nicholas Turner <bindlez@gmail.com>
2016-06-21 01:23:38 -0400

    commit 2016-06-21

--- leetcode/1880. Check if Word Equals Summation of Two Words.rb
+++ leetcode/1880. Check if Word Equals Summation of Two Words.rb
@@ -4,7 +4,34 @@ first_word = "acb"; second_word = "cba"; target_word = "cdb"
 # @param {String} second_word
 # @param {String} target_word
 # @return {Boolean}
+
+# p letters = ('a'..'z').to_a
+# p vals = (0..25).to_a
+# p map = letters.zip(vals).to_h
+# p map = Hash[letters.zip(vals)]
+# p [0,2,1].join.to_i
+#*
 def is_sum_equal(first_word, second_word, target_word)
-    p first_word.split('').map(&:ord)
+    letters = ('a'..'z').to_a
+    map = Hash[letters.zip(0..25)]
+    
+    a = first_word.chars.map { |l| map[l] }.join.to_i
+    b = second_word.chars.map { |l| map[l] }.join.to_i
+    c = target_word.chars.map { |l| map[l] }.join.to_i
+
+    a + b == c
 end
 p is_sum_equal(first_word, second_word, target_word)
+
+#* OG UNCLEAN CODE
+def is_sum_equal(first_word, second_word, target_word)
+    p letters = ('a'..'z').to_a
+    p map = letters.zip((0..25).to_a).to_h
+    p a = first_word.split('').map{|l| map[l]}.join('').sub(/^0+/, '').to_i
+    p b = second_word.split('').map{|l| map[l]}.join('').sub(/^0+/, '').to_i
+    p c = target_word.split('').map{|l| map[l]}.join('').sub(/^0+/, '').to_i
+
+    a + b == c ? true : false
+end
+p is_sum_equal(first_word, second_word, target_word)
+
