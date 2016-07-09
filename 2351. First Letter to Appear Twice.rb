736d0182f22379302e51d2d15e686850e5e8a43b
Nicholas Turner <bindlez@gmail.com>
2016-07-09 02:10:18 -0400

    commit 2016-07-09

--- 2351. First Letter to Appear Twice.rb
+++ 2351. First Letter to Appear Twice.rb
@@ -2,6 +2,7 @@
 s = "abccbaacz"
 # @param {String} s
 # @return {Character}
+#*
 def repeated_character(s)
     arr=[]
     s.chars.each do |c|
@@ -11,6 +12,7 @@ def repeated_character(s)
 end
 p repeated_character(s)
 
+#*
 def repeated_character(s)
     p tal = Hash.new(0)
     s.chars.each do |c|
@@ -20,3 +22,11 @@ def repeated_character(s)
     end
 end
 p repeated_character(s)
+
+#* OTHER PERSON SOL | EACH_WITH_OBJ=>HASH
+def repeated_character(s)
+    s.chars.each_with_object(Hash.new(0)) do |w, hash|
+      hash[w] += 1
+      return w if hash[w] > 1
+    end
+end
