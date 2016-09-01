5869ef44e669a38de9899f17b073cb0ecad337cb
Nicholas Turner <bindlez@gmail.com>
2016-08-31 22:17:33 -0400

    commit 2016-08-31

---
+++ 1725. Number Of Rectangles That Can Form The Largest Square.rb
@@ -0,0 +1,14 @@
+#* 1725. Number Of Rectangles That Can Form The Largest Square
+rectangles = [[5,8],[3,9],[5,12],[16,5]]
+# @param {Integer[][]} rectangles
+# @return {Integer}
+def count_good_rectangles(rectangles)
+  rectangles.map(&:min).count(rectangles.map(&:min).max)
+end
+p count_good_rectangles(rectangles)
+
+def count_good_rectangles(rectangles)
+  res=rectangles.map(&:min)
+  res.count(res.max)
+end
+p count_good_rectangles(rectangles)
