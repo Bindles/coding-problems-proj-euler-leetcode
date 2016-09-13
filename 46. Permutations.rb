b0b19717223a1a7e1195fcc09f2c7449adaf0045
Nicholas Turner <bindlez@gmail.com>
2016-09-13 14:04:16 -0400

    commit 2016-09-13

--- 46. Permutations.rb
+++ 46. Permutations.rb
@@ -2,7 +2,25 @@
 nums = [1,2,3]
 # @param {Integer[]} nums
 # @return {Integer[][]}
+#* MY SOL
 def permute(nums)
     nums.permutation(nums.size).to_a
+end
+p permute(nums)
+
+#* BACKTRACKING SOL
+def permute(nums)
+    result = []
+    backtrack(nums, [], result)
+    result
+end
+
+def backtrack(nums, array, result)
+    return result.push(array) if nums.empty?
+
+    nums.each do |n|
+        backtrack(nums - [n], [*array, n], result)
+    end
+    array
 end
 p permute(nums)
