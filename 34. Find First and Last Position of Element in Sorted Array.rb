781fa622c36d443f2cb0e1b9d89e13e0d4211850
Nicholas Turner <bindlez@gmail.com>
2017-03-10 01:58:10 -0500

    commit 2017-03-10

---
+++ 34. Find First and Last Position of Element in Sorted Array.rb
@@ -0,0 +1,19 @@
+#* 34. Find First and Last Position of Element in Sorted Array
+nums = [5,7,7,8,8,10]; target = 8
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+p indices = nums.select.with_index { |n, i| n == target }
+def search_range(nums, target)
+    nums.each_with_index.select{|n,i| i if n == target}
+end
+p search_range(nums, target)
+
+def search_range(nums, target)
+    res=[]
+    nums.each_with_index do |n,i|
+        res << i if n == target
+    end
+    res.empty? ? [-1, -1] : [res.first, res.last]
+end
+p search_range(nums, target)
---
+++ a.rb
@@ -0,0 +1,2 @@
+nums = [5,7,7,8,8,10]; target = 8
+p nums.select{|num| num == target}
