0dd2e8dc9f964fb3a009557baa941c345bbfa438
Nicholas Turner <bindlez@gmail.com>
2017-04-15 02:26:14 -0400

    commit 2017-04-15

--- 33. Search in Rotated Sorted Array.rb
+++ 33. Search in Rotated Sorted Array.rb
@@ -5,6 +5,13 @@ nums = [4,5,6,7,0,1,2]; target = 0
 # @return {Integer}
 #*
 def search(nums, target)
+  result = nums.sort.bsearch{ |n| target <=> n }
+  result.nil? ? -1 : nums.index(result)
+end
+p search(nums, target)
+
+#* 
+def search(nums, target)
   spaceship_operator_head_basher = ->(target, n) do
     return 0 if target == n
     return -1 if target < n
