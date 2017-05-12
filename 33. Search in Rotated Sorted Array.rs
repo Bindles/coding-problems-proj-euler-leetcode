dc2357c74bde222d991c82b8f5e4f01f9e5468e9
Nicholas Turner <bindlez@gmail.com>
2017-05-12 01:28:04 -0400

    commit 2017-05-12

--- 33. Search in Rotated Sorted Array.rs
+++ 33. Search in Rotated Sorted Array.rs
@@ -43,7 +43,7 @@ impl Solution {
 }
 
 
-//BSEARCH SOL RUST
+//BSEARCH SOL RUST | DOESNT WORK | COMING BACK DW
 impl Solution {
   pub fn search(nums: Vec<i32>, target: i32) -> i32 {
       let mut nums = nums;
@@ -53,6 +53,22 @@ impl Solution {
           index as i32
       } else {
           -1
+      }
+  }
+}
+
+
+//SOL 2 WORKS
+impl Solution {
+  pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+      let mut nums_with_indices: Vec<(i32, usize)> = nums.iter().enumerate().map(|(i, &val)| (val, i)).collect();
+      nums_with_indices.sort_unstable_by_key(|&(val, _)| val);
+
+      let result = nums_with_indices.binary_search_by_key(&target, |&(val, _)| val);
+
+      match result {
+          Ok(index) => nums_with_indices[index].1 as i32,
+          Err(_) => -1,
       }
   }
 }
