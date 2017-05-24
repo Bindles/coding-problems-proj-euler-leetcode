01d2bdf2277a1a4926196dc83598882a2d6f5e84
Nicholas Turner <bindlez@gmail.com>
2017-05-24 02:45:26 -0400

    commit 2017-05-24

--- 33. Search in Rotated Sorted Array.exs
+++ 33. Search in Rotated Sorted Array.exs
@@ -3,6 +3,6 @@ nums = [4,5,6,7,0,1,2]; target = 0
 defmodule Solution do
   @spec search(nums :: [integer], target :: integer) :: integer
   def search(nums, target) do
-    Enum.find_index(nums,&(&1 == target))
+    Enum.find_index(nums,&(&1 == target)) || -1
   end
 end
