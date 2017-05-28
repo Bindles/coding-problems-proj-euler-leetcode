9b01e2ec834eb61c919d1dc5d5b9a95d0fc8934f
Nicholas Turner <bindlez@gmail.com>
2017-05-28 01:03:42 -0400

    commit 2017-05-28

--- 33. Search in Rotated Sorted Array.exs
+++ 33. Search in Rotated Sorted Array.exs
@@ -1,5 +1,7 @@
+#ELIXIR
 #* 33. Search in Rotated Sorted Array
 nums = [4,5,6,7,0,1,2]; target = 0
+#SOL 1 | RUBY TRANSLATION | SUPER DUPER CONCISE | 100% | 266MS
 defmodule Solution do
   @spec search(nums :: [integer], target :: integer) :: integer
   def search(nums, target) do
