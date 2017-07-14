2077955bb895ddedfd6ccaab61102b6bbf6ee4cf
Nicholas Turner <bindlez@gmail.com>
2017-07-14 18:22:38 -0400

    commit 2017-07-14

---
+++ 2023. Number of Pairs of Strings With Concatenation Equal to Target.rb
@@ -0,0 +1,16 @@
+#* 2023. Number of Pairs of Strings With Concatenation Equal to Target
+nums = ["777","7","77","77"]; target = "7777"
+# @param {String[]} nums
+# @param {String} target
+# @return {Integer}
+#*
+def num_of_pairs(nums, target)
+    count=0
+    nums.each_index do |i|
+        (i...nums.size).each do |j|
+            count +=1 if [nums[i], nums[j]].join == target
+        end
+    end
+    count
+end
+p num_of_pairs(nums, target)
