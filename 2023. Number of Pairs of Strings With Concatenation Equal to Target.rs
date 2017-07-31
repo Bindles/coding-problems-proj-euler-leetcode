2fdaf99bcedd5db0b636dd7cdb6e3846b62bedbe
Nicholas Turner <bindlez@gmail.com>
2017-07-31 01:24:55 -0400

    commit 2017-07-31

--- 2023. Number of Pairs of Strings With Concatenation Equal to Target.rs
+++ 2023. Number of Pairs of Strings With Concatenation Equal to Target.rs
@@ -15,6 +15,37 @@ pub fn num_of_pairs(nums: Vec<String>, target: String) -> i32 {
 	count
 }
 
+//SOL 2 | HASHMAP
+pub fn num_of_pairs_map(nums: Vec<String>, target: String) -> i32 {
+	let mut map = std::collections::HashMap::new();
+	let mut count = 0;
+	let t_len = target.len();
+
+	for i in 0..nums.len() {
+		let num = nums[i].to_owned();
+		let n_len = num.len();
+
+		if target.starts_with(&num) {
+			let suffix = &target[n_len..];
+
+			if map.contains_key(suffix) {
+				count += map[suffix];
+			}
+		}
+
+		if target.ends_with(&num) {
+			let prefix = &target[..t_len - n_len];
+
+			if map.contains_key(prefix) {
+				count += map[prefix];
+			}
+		}
+
+		*map.entry(num).or_insert(0) += 1;
+	}
+	count
+}
+
 //WORKING . . .
 //DOESNT WORK | DIRECT RUBY TRANSLATION
 impl Solution {
