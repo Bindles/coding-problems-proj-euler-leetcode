64a3eae26a5b1915e34901e052c02abd0fb55e72
Nicholas Turner <bindlez@gmail.com>
2017-08-12 01:47:05 -0400

    commit 2017-08-12

--- 2023. Number of Pairs of Strings With Concatenation Equal to Target.py
+++ 2023. Number of Pairs of Strings With Concatenation Equal to Target.py
@@ -1,6 +1,12 @@
 #PYTHON
 #* 2023. Number of Pairs of Strings With Concatenation Equal to Target
 nums = ["777","7","77","77"]; target = "7777"
+#*
+def num_of_pairs(nums, target):
+    return sum(1 for i in range(len(nums)) for j in range(i, len(nums)) if nums[i] + nums[j] == target)
+
+print(num_of_pairs(nums, target))
+
 #* 
 def num_of_pairs(nums, target):
     return sum(1 for i in range(len(nums)) for j in range(i, len(nums)) if ''.join([nums[i], nums[j]]) == target)
