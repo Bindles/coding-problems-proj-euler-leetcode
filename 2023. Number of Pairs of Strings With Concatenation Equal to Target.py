18a762cb4005d4cad4ab279d831a6f1aba8e050c
Nicholas Turner <bindlez@gmail.com>
2017-08-18 01:11:00 -0400

    commit 2017-08-18

--- 2023. Number of Pairs of Strings With Concatenation Equal to Target.py
+++ 2023. Number of Pairs of Strings With Concatenation Equal to Target.py
@@ -1,6 +1,13 @@
 #PYTHON
 #* 2023. Number of Pairs of Strings With Concatenation Equal to Target
 nums = ["777","7","77","77"]; target = "7777"
+from typing import List
+#* SOL | OTHERS DONT WORK LOL WHY WONT RUBY LOOP ELIM CHECK i!=j
+class Solution:
+    def numOfPairs(self, nums: List[str], target: str) -> int:
+        return sum(1 for i in range(len(nums)) for j in range(len(nums)) if i!=j and nums[i]+nums[j]==target )
+
+print(Solution().numOfPairs(nums,target))   
 #*
 def num_of_pairs(nums, target):
     return sum(1 for i in range(len(nums)) for j in range(i, len(nums)) if nums[i] + nums[j] == target)
@@ -12,3 +19,4 @@ def num_of_pairs(nums, target):
     return sum(1 for i in range(len(nums)) for j in range(i, len(nums)) if ''.join([nums[i], nums[j]]) == target)
 
 print(num_of_pairs(nums, target))
+
