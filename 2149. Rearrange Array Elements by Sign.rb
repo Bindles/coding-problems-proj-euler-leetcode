dc28d15db58459f13643ba2f0c2459dcb1eb399a
Nicholas Turner <bindlez@gmail.com>
2017-08-26 02:22:41 -0400

    commit 2017-08-26

--- 2149. Rearrange Array Elements by Sign.rb
+++ 2149. Rearrange Array Elements by Sign.rb
@@ -3,11 +3,36 @@ nums = [3,1,-2,-5,2,-4]
 # [3,-2,1,-5,2,-4]
 # @param {Integer[]} nums
 # @return {Integer[]}
-#*
+#* SOL
 def rearrange_array(nums)
   positives, negatives = nums.partition(&:positive?)
   positives.zip(negatives).flatten
 end
+p  rearrange_array(nums)
+
+#* BETTER
+def rearrange_array(nums)
+  p nums.partition(&:positive?).transpose.flatten
+end
+p  rearrange_array(nums)
+
+#* DIFF WAY
+def rearrange_array(nums)
+  p results = Array.new(nums.size, 0)
+
+  index_even, index_odd = 0, 1
+  nums.each do |num|
+    if num > 0
+      results[index_even] = num
+      index_even += 2
+    else
+      results[index_odd] = num
+      index_odd += 2
+    end
+  end
+  results
+end
+p  rearrange_array(nums)
 
 #*WORKING . . . .
 #* DOES NOT WORK
