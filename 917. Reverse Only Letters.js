e693092ec2d820c23643abe5f213b3c26e3c0a83
Nicholas Turner <bindlez@gmail.com>
2017-09-08 02:10:49 -0400

    commit 2017-09-08

---
+++ 917. Reverse Only Letters.js
@@ -0,0 +1,20 @@
+// PYTHON
+// 917. Reverse Only Letters
+s = "ab-cd"
+/**
+ * @param {string} s
+ * @return {string}
+ */
+//SOL
+var reverseOnlyLetters = function(s) {
+    // Collect all the letters in the string
+    let letters = s.split('').filter(c => /[a-zA-Z]/.test(c));
+    // Replace letters in the string with those from the collected letters in reverse order
+    let result = s.split('').map(c => /[a-zA-Z]/.test(c) ? letters.pop() : c).join('');
+    return result;
+}
+
+// Test cases
+console.log(reverseOnlyLetters("ab-cd"));          // Output: "dc-ba"
+console.log(reverseOnlyLetters("a-bC-dEf-ghIj"));  // Output: "j-Ih-gfE-dCba"
+console.log(reverseOnlyLetters("Test1ng-Leet=code-Q!"));  // Output: "Qedo1ct-eeLg=ntse-T!"
