255eb8e3be45dfcf9356ffad464d0128d77ddb3d
Nicholas Turner <bindlez@gmail.com>
2017-09-13 00:58:45 -0400

    commit 2017-09-13

--- 917. Reverse Only Letters.py
+++ 917. Reverse Only Letters.py
@@ -1,6 +1,17 @@
 # PYTHON
 #* 917. Reverse Only Letters
 s = "ab-cd"
+#*
+def reverse_only_letters(s):
+    letters = [c for c in s if c.isalpha()]
+    result = ''.join([letters.pop() if c.isalpha() else c for c in s])
+    return result
+
+print(reverse_only_letters("ab-cd"))          # Output: "dc-ba"
+print(reverse_only_letters("a-bC-dEf-ghIj"))  # Output: "j-Ih-gfE-dCba"
+print(reverse_only_letters("Test1ng-Leet=code-Q!"))  # Output: "Qedo1ct-eeLg=ntse-T!"
+
+#* W COMMENTS
 def reverse_only_letters(s):
     # Collect all the letters in the string
     letters = [c for c in s if c.isalpha()]
