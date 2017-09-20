610793276f8955cfd125ffe37835ba2d70de9026
Nicholas Turner <bindlez@gmail.com>
2017-09-20 01:08:52 -0400

    commit 2017-09-20

---
+++ 41. First Missing Positive.py
@@ -0,0 +1,24 @@
+# PYTHON
+#* 41. First Missing Positive
+nums = [1,2,0]
+from typing import List
+#*
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = n + 1
+        
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        
+        return n + 1
+    
+print(Solution().firstMissingPositive(nums))
