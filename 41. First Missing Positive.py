3c52167c53c0c4081aea014cc98543e738cf919c
Nicholas Turner <bindlez@gmail.com>
2017-09-21 00:38:24 -0400

    commit 2017-09-21

--- 41. First Missing Positive.py
+++ 41. First Missing Positive.py
@@ -22,3 +22,28 @@ class Solution:
         return n + 1
     
 print(Solution().firstMissingPositive(nums))
+    
+
+#*
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        # Replace negative numbers, zeros, and numbers larger than n with n+1
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = n + 1
+        
+        # Place each number in its correct position
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        
+        # Find the first missing positive
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        
+        return n + 1
+    
+print(Solution().firstMissingPositive(nums))
