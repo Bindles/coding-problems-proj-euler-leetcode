61a3652c9fac022654097c815947437f989d9329
Nicholas Turner <bindlez@gmail.com>
2017-10-04 01:59:43 -0400

    commit 2017-10-04

--- 2317. Maximum XOR After Operations.rb
+++ 2317. Maximum XOR After Operations.rb
@@ -2,7 +2,18 @@
 nums = [3,2,4,6]
 # @param {Integer[]} nums
 # @return {Integer}
+#*
 def maximum_xor(nums)
   nums.reduce(0){|acc,num| acc | num} 
+end
+p maximum_xor(nums)
+
+#*
+def maximum_xor(nums)
+  acc = 0
+  nums.each do |num|
+    acc |= num
+  end
+  acc
 end
 p maximum_xor(nums)
