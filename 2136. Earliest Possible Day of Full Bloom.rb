887974bd2fa12e7adc7b290dbaa7f41b46fd1b23
Nicholas Turner <bindlez@gmail.com>
2017-10-11 01:59:36 -0400

    commit 2017-10-11

--- 2136. Earliest Possible Day of Full Bloom.rb
+++ 2136. Earliest Possible Day of Full Bloom.rb
@@ -24,4 +24,11 @@ def earliest_full_bloom(plant_time, grow_time)
 end
 p earliest_full_bloom(plant_time, grow_time)
 
+#*
+def earliest_full_bloom(plant_time, grow_time)
+  plant_time.zip(grow_time).sort {|a, b| b.last <=> a.last }.inject([0, 0]) {|(tcur, tend), (x, g)|
+      [tcur + x, [tend, tcur + x + g].max]
+  }.last
+end
+p earliest_full_bloom(plant_time, grow_time)
 
