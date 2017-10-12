66ae36153be874d5b0fd4e72a68bb07d1bc97dd8
Nicholas Turner <bindlez@gmail.com>
2017-10-12 02:19:27 -0400

    commit 2017-10-12

--- 2136. Earliest Possible Day of Full Bloom.rb
+++ 2136. Earliest Possible Day of Full Bloom.rb
@@ -5,15 +5,11 @@ plant_time = [1,4,3]; grow_time = [2,3,1]
 # @return {Integer}
 #*
 def earliest_full_bloom(plant_time, grow_time)
-  rest_day = 0
-  more_necessary_days = []
+  rest_day, more_necessary_days = 0, []
   plant_time.zip(grow_time).sort_by(&:last).each do |plant_day, grow_day|
-  
     more_necessary_days << (grow_day - rest_day) if  rest_day < grow_day
-	
     rest_day += plant_day
   end
-  
   plant_time.sum + more_necessary_days.max
 end
 p earliest_full_bloom(plant_time, grow_time)
