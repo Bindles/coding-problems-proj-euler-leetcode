b145a55a8b9c06e2a2fb6f441981d8c976802d61
Nicholas Turner <bindlez@gmail.com>
2017-10-22 01:36:15 -0400

    commit 2017-10-22

---
+++ 1304. Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,14 @@
+#* 1304. Find N Unique Integers Sum up to Zero
+n = 5
+def sum_zero(n):
+    result = []
+    if n % 2 != 0:
+        result.append(0)
+    for i in range(1, n // 2 + 1):
+        result.append(i)
+        result.append(-i)
+    return result
+
+# Test case
+n = 5
+print(sum_zero(n))
