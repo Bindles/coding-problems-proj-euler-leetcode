3fa80db1e250b0d4b9720ca73d9af6453f4142de
Nicholas Turner <bindlez@gmail.com>
2017-11-21 00:36:37 -0500

    commit 2017-11-21

--- 2444. Count Subarrays With Fixed Bounds.rb
+++ 2444. Count Subarrays With Fixed Bounds.rb
@@ -35,4 +35,40 @@ def count_subarrays(nums, *b) =
           subarray.zip(1..).sum { |element, index| index_hash[element] = index and bounds.map { |bound| index_hash[bound] || 0 }.min }
         }
   end
+
+  #*
+  def count_subarrays(nums, *bounds)
+    nums.chunk { [*bounds, num].minmax == bounds }
+        .select(&:shift)
+        .sum { |subarray, index_hash = {}|
+          subarray.zip(1..).sum { |element, index| index_hash[element] = index and bounds.map { |bound| index_hash[bound] || 0 }.min }
+        }
+  end
+
+
+  #* 2 FS
+  def count_subarrays(nums, *b)
+    nums.chunk { (b[0]..b[1]) === _1 }
+        .select(&:shift)
+        .select { _1[0].minmax == b }
+        .sum { |a, h = b.to_h { [_1, 0] }|
+          a.zip(1..).sum { |x, i| h[x] &&= i
+          h.values.min }
+        }
+  end
+
+  
+  #*
+  def count_subarrays(nums, *bounds)
+    nums.chunk { |num| (bounds[0]..bounds[1]) === num }
+        .select(&:shift)
+        .select { |subarray| subarray[0].minmax == bounds }
+        .sum { |subarray, index_hash = bounds.to_h { |bound| [bound, 0] }|
+          subarray.zip(1..).sum { |element, index| 
+            index_hash[element] &&= index
+            index_hash.values.min 
+          }
+        }
+  end
+  
   
