b98729f21b77a79ca5f63bc7264cc5945f1cc189
Nicholas Turner <bindlez@gmail.com>
2017-12-06 00:41:09 -0500

    commit 2017-12-06

--- 1450. Number of Students Doing Homework at a Given Time.rb
+++ 1450. Number of Students Doing Homework at a Given Time.rb
@@ -1,9 +1,10 @@
 #* 1450. Number of Students Doing Homework at a Given Time
-startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+start_time = [1,2,3]; end_time = [3,2,7]; query_time = 4
 # @param {Integer[]} start_time
 # @param {Integer[]} end_time
 # @param {Integer} query_time
 # @return {Integer}
+#*
 def busy_student(start_time, end_time, query_time)
     times = start_time.zip(end_time)
     times.map{|a,b| (a..b).to_a}.count{|pair| pair.include?(query_time)}
