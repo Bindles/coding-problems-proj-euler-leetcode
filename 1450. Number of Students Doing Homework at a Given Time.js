72f1deea5717e72ea3a7b67d1593958ab22a727d
Nicholas Turner <bindlez@gmail.com>
2017-12-13 01:43:28 -0500

    commit 2017-12-13

--- 1450. Number of Students Doing Homework at a Given Time.js
+++ 1450. Number of Students Doing Homework at a Given Time.js
@@ -23,10 +23,7 @@ console.log(busyStudent(startTime, endTime, queryTime))
 
 // SOL
 var busyStudent = function(startTime, endTime, queryTime) {
-    // Zip startTime and endTime into pairs
     const times = startTime.map((start, index) => [start, endTime[index]]);
-
-    // Count how many pairs include queryTime directly
     const count = times.filter(([a, b]) => queryTime >= a && queryTime <= b).length;
 
     return count;
@@ -34,9 +31,3 @@ var busyStudent = function(startTime, endTime, queryTime) {
 console.log(busyStudent(startTime, endTime, queryTime))
 
 
-// #*
-// def busy_student(start_time, end_time, query_time)
-//     times = start_time.zip(end_time)
-//     times.map{|a,b| (a..b).to_a}.count{|pair| pair.include?(query_time)}
-// end
-// p busy_student(start_time, end_time, query_time) 
