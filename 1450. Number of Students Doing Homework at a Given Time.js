f97d4f88ab3946baaff240db75d8f6cfba1bee88
Nicholas Turner <bindlez@gmail.com>
2017-12-15 00:22:43 -0500

    commit 2017-12-15

--- 1450. Number of Students Doing Homework at a Given Time.js
+++ 1450. Number of Students Doing Homework at a Given Time.js
@@ -30,4 +30,19 @@ var busyStudent = function(startTime, endTime, queryTime) {
 }
 console.log(busyStudent(startTime, endTime, queryTime))
 
+// SOL
+var busyStudent = function(startTime, endTime, queryTime) {
+    let count = 0;
+
+    for (let i = 0; i < startTime.length; i++) {
+        if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+console.log(busyStudent(startTime, endTime, queryTime))
+
+
 
