630d5a70b3d8f10836899ff8e294e88ab55e8360
Nicholas Turner <bindlez@gmail.com>
2017-12-25 00:04:02 -0500

    commit 2017-12-25

---
+++ 2428. Maximum Sum of an Hourglass.py
@@ -0,0 +1,13 @@
+# PYTHON
+#* 2428. Maximum Sum of an Hourglass
+grid = [[6,2,1,3],[4,2,1,5],[9,2,8,7],[4,1,2,9]]
+# @param {Integer[][]} grid
+# @return {Integer}
+#*
+def max_sum(grid):
+  return max(grid[i][j] + grid[i][j+1] + grid[i][j+2] +
+    grid[i+1][j+1] +
+    grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]
+    for i in range(len(grid)-2) for j in range(len(grid[0])-2))
+
+print(max_sum(grid))
