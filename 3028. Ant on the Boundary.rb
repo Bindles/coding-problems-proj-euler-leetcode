c4396d64713335504eda10bbeb49c147a019ab1a
Nicholas Turner <bindlez@gmail.com>
2018-01-01 02:50:40 -0500

    commit 2018-01-01

--- 3028. Ant on the Boundary.rb
+++ 3028. Ant on the Boundary.rb
@@ -2,6 +2,7 @@
 nums = [2,3,-5]
 # @param {Integer[]} nums
 # @return {Integer}
+#*
 def return_to_boundary_count(nums)
   total = boundary_hits = 0
   nums.each do |num|
@@ -9,5 +10,11 @@ def return_to_boundary_count(nums)
     boundary_hits += 1 if total == 0
   end
   boundary_hits
+end
+p return_to_boundary_count(nums)
+
+#*
+def return_to_boundary_count(nums)
+  nums.inject([0, 0]) { |(total, boundary_hits), num| [total + num, boundary_hits + (total + num == 0 ? 1 : 0)] }[1]
 end
 p return_to_boundary_count(nums)
