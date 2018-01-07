83a8630b9236b1b7f483a6c42d0bd187710b433a
Nicholas Turner <bindlez@gmail.com>
2018-01-07 02:53:31 -0500

    commit 2018-01-07

---
+++ 3028. Ant on the Boundary.rs
@@ -0,0 +1,17 @@
+// 3028. Ant on the Boundary
+// nums = [2,3,-5]
+
+impl Solution {
+    pub fn return_to_boundary_count(nums: Vec<i32>) -> i32 {
+        let (mut total, mut boundary_hits) = (0, 0);
+
+        for &num in nums.iter() {
+            total += num;
+            if total == 0 {
+                boundary_hits += 1;
+            }
+        }
+
+        boundary_hits
+    }
+}
